785475b3c741950f482751ca31d86f535396d8c5
MatthewFBullock <MatthewFBullock@users.noreply.github.com>
2019-06-04 22:14:30 -0700

    rearranging stuff

---
+++ index.js
@@ -0,0 +1,18 @@
+// load our app server using express
+const express = require('express');
+const app = express();
+
+// app.get("/", (req, res) => {
+//     console.log("Responding to root route");
+//     res.send("Hello from route0000001");
+// });
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/style.css');
+})
+
+app.listen(8080, () => {
+    console.log("Server is up and listening on 8080...");
+});
+
