b402fc4f854d0711db431a2f7228107fc5914138
MatthewFBullock <MatthewFBullock@users.noreply.github.com>
2019-06-10 23:07:55 -0700

    Updated to live version

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+// import $ from 'jquery';
+// import 'is-in-viewport';
+
+$(function(){
+    // $('video').each(function(){
+    //     if ($(this).is(":in-viewport")) {
+    // $(this)[0].play();
+    // } else {
+    // $(this)[0].pause();
+    //     }
+    // });
+    // $('h1:in-viewport').css('background-color', 'red');
+
+    var headshot = document.getElementById('headshot');
+    $(headshot).hide().slideDown('slow');
+    
+    
+});
